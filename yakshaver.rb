2f3a343ed57443f4256d4f0474cae7dcd6b16220
Chris Metcalf <chris@chrismetcalf.net>
2012-12-08 13:48:46 -0800

    Added the ribbon

--- yakshaver.rb
+++ yakshaver.rb
@@ -18,6 +18,8 @@ __END__
     <title>No shaving necessary!</title>
   </head>
   <body>
+    <a href="https://github.com/chrismetcalf/yakshaver"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a>
+
     <h1>Hair free yaks, on Cloudfoundry!</h1>
 
     <img src="http://25.media.tumblr.com/tumblr_mbytf03SYe1qcj6ego1_500.jpg" alt="Clean as a baby yak" />
